88caeebc4fd43a4ef7734a2615b0dd099e375539
Enrique Sierra Gutiérrez <enrique.sierra.0000@gmail.com>
2021-11-17 20:14:22 -0600

    Create hollow automapper tests class

---
+++ tests/BookingX.Core.Application.Tests/AutomapperTests.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingX.Core.Application.Tests
+{
+    // TODO: Implement automapper tests
+    public class AutomapperTests
+    {
+        
+    }
+}
